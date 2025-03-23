d36ecc30c360542e6ce2803b015062ae91d97537
Ernest luthius <eluthius990@gmail.com>
2025-03-23 19:39:56 +0300

    created new files
    commercial.html
    residential.html

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
+
 // Initialize AOS
 AOS.init({
     duration: 1000,
     once: true,
   });
+
+    // Toggle Mobile Menu
+    const menuToggle = document.getElementById('menu-toggle');
+    const mobileMenu = document.getElementById('mobile-menu');
+  
+    menuToggle.addEventListener('click', () => {
+      mobileMenu.classList.toggle('hidden');
+    });
   
   // Sticky Navbar
   window.addEventListener('scroll', () => {
@@ -13,23 +22,3 @@ AOS.init({
       nav.classList.remove('scrolled');
     }
   });
-
-  // Toggle Mobile Menu
-  const menuToggle = document.getElementById('menu-toggle');
-  const mobileMenu = document.getElementById('mobile-menu');
-
-  menuToggle.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-  });
-
-  // GSAP Animations for Services List
-gsap.from(".services-list li", {
-  opacity: 0,
-  y: 50,
-  stagger: 0.2,
-  duration: 1,
-  scrollTrigger: {
-    trigger: ".services-list",
-    start: "top 80%",
-  },
-});
